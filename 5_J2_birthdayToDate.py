5befc05fb9ea7044e8a90b2d4ef0f9f41e3d176a
Marian Simon <93652525+mariiansiimon@users.noreply.github.com>
2022-12-25 14:37:03 +0330

    Update 5_J2_birthdayToDate.py

--- 5_J2_birthdayToDate.py
+++ 5_J2_birthdayToDate.py
@@ -1,21 +1,9 @@
-from calendar import month_abbr
 from datetime import datetime as dt
 import jalali
 
-year1 = int( input( 'Year : ' ) )
-month1 = int( input( 'Month : ' ) )
-day1 = int( input( ( 'Day : ' ) ) ) 
-print( 'Your Birthday is', year1,'/',month1,'/',day1 )
-
-sal = jalali.Gregorian(dt.today().year, dt.today().month , dt.today().day ).persian_string()
-
-
-shamsY = int( sal[:4] ) 
-shamsM = int( sal[5:6] )
-shamsD = int( sal[7:9] )
-
-print(shamsY, shamsM, shamsD)
-
-birthday =  (shamsY - year1 ) *365 + (shamsM - month1)*30 + shamsD - day1    
-
-print( f'you lived { birthday } days! ' )
+bd = input('Please enter your birthday like 1393-24-3 : ')
+bd = jalali.persian(bd).gregorian_string()
+bd = datetime.strptime(bd, '%y-%m-%d')
+today = datetime.today()
+delta = today - bd
+print(delta)
