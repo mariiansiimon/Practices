e1bfa8dd66072276a54b6adddd47e4981e76cf82
Marian Simon <93652525+mariiansiimon@users.noreply.github.com>
2022-12-27 18:52:17 +0330

    Add files via upload

---
+++ 6_J3_fardMazrab5.py
@@ -0,0 +1,22 @@
+# برنامه ای بنویسید یک عدد صحیح از کاربر بگیرد و مجموع اعداد فرد از صفر تا آن عدد را محاسبه کرده و در مجموع اعداد مضرب 5 از صفر تا آن عدد ضرب کند.
+
+# number / 0+1+3+5+7....+number (number odd or even ) 
+
+num = int( input( 'Number : ' ))
+jam = 0
+
+if num % 2 == 0 :
+    for i in range(1, num, 2):
+        jam += i
+    print(jam)
+
+else:
+    for i in range(1, num + 1, 2):
+        jam += i
+    print(jam)
+
+
+for i in range(0, num + 1 , 5) :
+    jam += i
+    print(i)
+print(jam)
