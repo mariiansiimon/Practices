afc6ca530cf7a7770cb62cabb3921c3470feb095
Marian Simon <93652525+mariiansiimon@users.noreply.github.com>
2022-12-27 18:42:52 +0330

    6_2_J3_myName.py

---
+++ 2_J3_myName.py
@@ -0,0 +1,24 @@
+# اسم خودتان را در یک متغییر بنویسید و برعکس کنید ( از دو روش استفاده کنید 1- از روش اسلایسینگ 2- از طریق حلقه ها)
+
+# FIRST 
+
+name = 'marian'
+newName = name[::-1]
+print(name ,'/', newName)
+
+
+# second
+
+name1= 'simon'
+
+s = -1
+for i in name1:
+    s *= i
+    print(i)
+
+
+
+
+
+
+
